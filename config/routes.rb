0042557a8a60361b5d9bd5b2fb49caf8723d72ad
mkita36 <61313424+mkita36@users.noreply.github.com>
2020-03-31 20:57:26 +0900

    ログイン機能を実装

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'tweets#index'
+  get 'tweets/index'
+  get 'tweets/new'
+  get 'tweets/show'
+  get 'tweets/create'
+  get 'tweets/update'
+  get 'tweets/destroy'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
