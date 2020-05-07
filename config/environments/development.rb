ea8e3d10a399b5af5e442296cec5358e00b41707
mkita36 <61313424+mkita36@users.noreply.github.com>
2020-05-07 23:08:37 +0900

    メール認証機能実装

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,11 +64,11 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :user_name => "sacred0880dercas@gmail.com",
-    :password => "kvuf eiqh myrr ozkj",
-    :authentication => :plain,
-    :enable_starttls_auto => true
+    address: "smtp.gmail.com",
+    port: 587,
+    user_name: ENV['SMTP_USER_NAME'],
+    password: ENV['SMTP_PASSWORD'],
+    authentication: :plain,
+    enable_starttls_auto: true
   }
 end
